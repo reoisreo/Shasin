409496a3a853b8f20627af753d358d1d5227ea73
リ　ユウテイ <leo@MacBook-Air-8.local>
2026-04-24 00:04:22 +0900

    New Project

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'shashins-v1';
+const FILES = ['./shashins.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
