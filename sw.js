7d225eb88b4dbe05560a6ee08cb762069416fe38
リ　ユウテイ <leo@MacBook-Air-8.local>
2026-04-24 00:29:48 +0900

    cache bump update

--- sw.js
+++ sw.js
@@ -1,12 +1,21 @@
-const CACHE = 'shashins-v1';
-const FILES = ['./shashins.html', './manifest.json'];
+const CACHE = 'shashins-v2';
+const FILES = ['./manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  if (e.request.url.endsWith('.html')) {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  } else {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
 });
